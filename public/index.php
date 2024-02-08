93c5e3eac675e7b29051f809443a0e2e7b700cad
Wadson Pinheiro <94648773+Wadsom@users.noreply.github.com>
2024-02-08 00:47:18 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -20,26 +20,6 @@ else if ($_SERVER['PATH_INFO'] === '/novo-video') {
 } else if ($_SERVER['PATH_INFO'] === '/remover-video') {
     require_once __DIR__ . '/../removendo-video.php';
 }
-//
-//if (!array_key_exists('PATH_INFO', $_SERVER) || $_SERVER['PATH_INFO'] === '/') {
-//    require_once __DIR__ . '/../listagem-video.php';
-//}
-//else if ($_SERVER['PATH_INFO'] === '/novo-video') {
-//    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-//        require_once __DIR__ . '/../formulario.php';
-//    } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//        require_once __DIR__ . '/../video.php';
-//    }
-//} else if ($_SERVER['PATH_INFO'] === '/editar-video') {
-//    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-//        require_once __DIR__ . '/../formulario.php';
-//    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//        require_once __DIR__ . '/../editar-video.php';
-//    }
-//} else if ($_SERVER['PATH_INFO'] === '/remover-video') {
-//    require_once __DIR__ . '/../removendo-video.php';
-//}
-
 
 
 
